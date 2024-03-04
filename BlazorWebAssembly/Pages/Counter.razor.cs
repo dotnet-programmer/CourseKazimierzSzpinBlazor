fd062ce28b09c63af9f6543ac5929c500a5808eb
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-03-04 19:53:04 +0100

    WASM - segregation of responsibility - separate HTML and C#

---
+++ BlazorWebAssembly/Pages/Counter.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorWebAssembly.Pages;
+
+public partial class Counter
+{
+	private int _currentCount = 0;
+	private bool _isActive = false;
+
+	private void IncrementCount()
+		=> _currentCount++;
+
+	private void ToggleActive()
+		=> _isActive = !_isActive;
+}
