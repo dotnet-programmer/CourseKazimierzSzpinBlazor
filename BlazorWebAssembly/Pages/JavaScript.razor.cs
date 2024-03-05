6024c811d9c083e755b52c51f4720d61ee02a379
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-03-05 15:07:27 +0100

    WASM - using JavaScript in Blazor

---
+++ BlazorWebAssembly/Pages/JavaScript.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorWebAssembly.Pages;
+
+public partial class JavaScript
+{
+	[Inject]
+	public IJSRuntime JSRuntime { get; set; }
+
+	// wywołanie funkcji JS bez zwracanych danych
+	private async Task DisplayAlert()
+	{
+		// pierwszy parametr to nazwa funkcji JS która ma zostać wywołana, a kolenje to lista parametrów przekazanych do tej metody
+		await JSRuntime.InvokeVoidAsync("alert", "Przykładowa wiadomość");
+	}
+
+	// wywołanie funkcji JS ze zwracaniem danych
+	private bool _dialogResult = false;
+	private async Task DisplayConfirmDialog ()
+	{
+		// TValue oznacza typ wartości zwracanej
+		_dialogResult = await JSRuntime.InvokeAsync<bool>("confirm", "Czy na pewno chcesz usunąć rekord?");
+	}
+
+	// wywołanie własnej funkcji napisanej w JS
+	private async Task DisplayResultJS()
+	{
+		await JSRuntime.InvokeVoidAsync("addNumberJS", 1, 2);
+	}
+}
---
+++ BlazorWebAssembly/wwwroot/scripts/jsFunctions.js
@@ -0,0 +1,4 @@
+function addNumberJS(number1, number2) {
+	var sum = number1 + number2;
+	alert(sum);
+}
