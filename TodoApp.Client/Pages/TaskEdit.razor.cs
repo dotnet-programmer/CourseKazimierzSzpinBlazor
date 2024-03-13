a6b18dc1f65b0167da00ea470e9ab0ed965c9b93
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-03-13 19:19:27 +0100

    TodoApp - update

---
+++ TodoApp.Client/Pages/TaskEdit.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using TodoApp.Client.HttpRepository.Interfaces;
+using TodoApp.Client.Services.Interfaces;
+using TodoApp.Shared.Tasks.Commands;
+
+namespace TodoApp.Client.Pages;
+
+public partial class TaskEdit
+{
+	private bool _isLoading = false;
+	private EditTaskCommand _editTaskCommand = new() { Term = DateTime.Now };
+
+	[Parameter]
+	public int Id { get; set; }
+
+	[Inject]
+	public ITaskHttpRepository TaskHttpRepository { get; set; }
+
+	[Inject]
+	public NavigationManager NavigationManager { get; set; }
+
+	[Inject]
+	public IToastrService ToastrService { get; set; }
+
+	protected override async Task OnInitializedAsync()
+	{
+		_editTaskCommand = await TaskHttpRepository.GetEdit(Id);
+	}
+
+	private async Task Save()
+	{
+		try
+		{
+			_isLoading = true;
+			await TaskHttpRepository.Edit(_editTaskCommand);
+			NavigationManager.NavigateTo("/");
+			await ToastrService.ShowSuccessMessage("Zadanie zostało zaktualizowane.");
+		}
+		finally
+		{
+			_isLoading = false;
+		}
+	}
+}
