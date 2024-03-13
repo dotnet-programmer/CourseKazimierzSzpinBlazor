5c6a554a0a0c0771d1962cbf383b86352b9cc350
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-03-13 19:00:22 +0100

    TodoApp - read

---
+++ TodoApp.Client/Pages/Tasks.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using TodoApp.Client.HttpRepository.Interfaces;
+using TodoApp.Shared.Tasks.Dtos;
+
+namespace TodoApp.Client.Pages;
+
+public partial class Tasks
+{
+	private IList<TaskDto> _tasks;
+
+	[Inject]
+	public ITaskHttpRepository TaskHttpRepository { get; set; }
+
+	protected override async Task OnInitializedAsync()
+	{
+		_tasks = await TaskHttpRepository.GetAll();
+	}
+}
