ad1b6b28ca1295892befd991da1e059e58ca1a62
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-03-15 15:43:33 +0100

    SimpleShop - component Product

---
+++ SimpleShop/SimpleShop.Client/Shared/Product.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using SimpleShop.Shared.Products.Dtos;
+
+namespace SimpleShop.Client.Shared;
+
+public partial class Product
+{
+	private string _baseUrl = string.Empty;
+
+	[Parameter]
+	public ProductDto ProductModel { get; set; }
+
+	[Inject]
+	public NavigationManager NavigationManager { get; set; }
+
+	[Inject]
+	public IConfiguration Configuration { get; set; }
+
+	protected override async Task OnInitializedAsync() 
+		=> _baseUrl = Configuration["ApiConfiguration:BaseAddress"];
+
+	private async Task OnAddProductToCart()
+	{
+		//
+	
+		NavigationManager.NavigateTo("/koszyk");
+	}
+}
