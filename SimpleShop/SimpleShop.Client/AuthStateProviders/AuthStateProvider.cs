7747e0cba29912b5c96f842197aa3a0920b14efa
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-03-16 15:53:16 +0100

    SimpleShop - AuthenticationStateProvider

--- SimpleShop/SimpleShop.Client/AuthStateProviders/AuthStateProvider.cs
+++ SimpleShop/SimpleShop.Client/AuthStateProviders/AuthStateProvider.cs
@@ -1,24 +1,47 @@
-﻿using System.Security.Claims;
+﻿using System.Net.Http.Headers;
+using System.Security.Claims;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
+using SimpleShop.Client.Models;
 
 namespace SimpleShop.Client.AuthStateProviders;
 
 // klasa z definicją jak ma działać uwierzytelnianie w aplikacji
 public class AuthStateProvider : AuthenticationStateProvider
 {
+	private readonly HttpClient _httpClient;
+	private readonly ILocalStorageService _localStorage;
+	private readonly AuthenticationState _anonymous;
+
+	public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+	{
+		_httpClient = httpClient;
+		_localStorage = localStorage;
+		_anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+	}
+
 	public override async Task<AuthenticationState> GetAuthenticationStateAsync()
 	{
-		//sztuczny użytkownik do testów
-		var anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
-		//return await Task.FromResult(anonymous);
-		var claims = new List<Claim>
+		////sztuczny użytkownik do testów
+		//var anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+		////return await Task.FromResult(anonymous);
+		//var claims = new List<Claim>
+		//{
+		//	new(ClaimTypes.Name, "k.szpin@wp.pl"),
+		//	new(ClaimTypes.Role, "Administrator")
+		//};
+		//var auth = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims, "test")));
+		//// zwrócenie informacji o tym czy użytkownik zalgowany czy nie
+		//return await Task.FromResult(auth);
+
+		var token = await _localStorage.GetItemAsync<string>("authToken");
+
+		if (string.IsNullOrWhiteSpace(token))
 		{
-			new(ClaimTypes.Name, "k.szpin@wp.pl"),
-			new(ClaimTypes.Role, "Administrator")
-		};
-		var auth = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims, "test")));
-		// zwrócenie informacji o tym czy użytkownik zalgowany czy nie
-		return await Task.FromResult(auth);
+			return _anonymous;
+		}
+
+		_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+		return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType")));
 	}
 }
