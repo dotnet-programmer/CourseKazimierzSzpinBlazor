fea0d3c37bf692194b1d87ae92e61ae23b57fb12
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-03-18 18:28:33 +0100

    WASM Syncfusion - add Syncfusion lib

--- BlazorWebAssemblySyncfusion/Program.cs
+++ BlazorWebAssemblySyncfusion/Program.cs
@@ -1,11 +1,16 @@
 using BlazorWebAssemblySyncfusion;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+using Syncfusion.Licensing;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSyncfusionBlazor();
+SyncfusionLicenseProvider.RegisterLicense("");
 
 await builder.Build().RunAsync();
