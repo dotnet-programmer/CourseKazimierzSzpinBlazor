9690484aaaed1ad77a258ecf38f8167f54357c9f
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2024-03-18 16:03:57 +0100

    WASM Radzen - DataGrid

---
+++ BlazorWebAssemblyRadzen/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorWebAssemblyRadzen.Models;
+
+public class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+}
